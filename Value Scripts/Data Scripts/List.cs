9d832fb31b1233c83f407f5bd216098c2b20f181
xXrederXx <150281252+xXrederXx@users.noreply.github.com>
2024-11-05 12:52:15 +0100

    Update List.cs

--- Value Scripts/Data Scripts/List.cs
+++ Value Scripts/Data Scripts/List.cs
@@ -1,6 +1,6 @@
 namespace YSharp_2._0;
 
-public class List(List<Value> elements) : Value
+public class VList(List<Value> elements) : Value
 {
     public List<Value> elements = elements;
 
@@ -8,23 +8,23 @@ public class List(List<Value> elements) : Value
     {
         if (name == "Count")
         {
-            return (new Number(elements.Count), NoError.Instance);
+            return (new VNumber(elements.Count), ErrorNull.Instance);
         }
         return base.GetVar(name);
     }
-    private (int, Error) convertToCSindex(List<Value> argValues)
+    private (int, Error) ConvertToCSindex(List<Value> argValues)
     { // converts a value to a csharp usable index
-        Error err = CheckArgs(argValues, 1, [typeof(Number)]);
+        Error err = ValueHelper.CheckArgs(argValues, 1, [typeof(VNumber)], context);
         if (err.IsError)
         {
             return (0, err);
         }
 
-        int index = (int)((Number)argValues[0]).value;
+        int index = (int)((VNumber)argValues[0]).value;
 
         if (index >= elements.Count)
         {
-            return (0, new ArgOutOfRangeError(argValues[0].startPos, "Index was out of range. Must be less than size of list."));
+            return (0, new ArgOutOfRangeError(argValues[0].startPos, "Index was out of range. Must be less than size of list.", context));
         }
 
         if (index < 0)
@@ -34,88 +34,88 @@ public class List(List<Value> elements) : Value
 
         if (index < 0)
         {
-            return (0, new ArgOutOfRangeError(argValues[0].startPos, "Index was out of range. Negative size cant be greater than size of list."));
+            return (0, new ArgOutOfRangeError(argValues[0].startPos, "Index was out of range. Negative size cant be greater than size of list.", context));
         }
 
-        return (index, NoError.Instance);
+        return (index, ErrorNull.Instance);
     }
     public override ValueError GetFunc(string name, List<Value> argValues)
     {
         if (name == "Add")
         {
             elements.AddRange(argValues);
-            return (ValueNull.Instance, NoError.Instance);
+            return (ValueNull.Instance, ErrorNull.Instance);
         }
         else if (name == "Get")
         {
-            (int, Error) index = convertToCSindex(argValues);
+            (int, Error) index = ConvertToCSindex(argValues);
             if (index.Item2.IsError)
             {
                 return (ValueNull.Instance, index.Item2);
             }
 
-            return (elements[index.Item1], NoError.Instance);
+            return (elements[index.Item1], ErrorNull.Instance);
         }
         else if (name == "Remove")
         {
-            (int, Error) index = convertToCSindex(argValues);
+            (int, Error) index = ConvertToCSindex(argValues);
             if (index.Item2.IsError)
             {
                 return (ValueNull.Instance, index.Item2);
             }
 
             elements.RemoveAt(index.Item1);
-            return (ValueNull.Instance, NoError.Instance);
+            return (ValueNull.Instance, ErrorNull.Instance);
         }
         else if (name == "IndexOf")
         { // returns -1 if not found
-            Error err = IsRightLength(1, argValues);
+            Error err = ValueHelper.IsRightLength(1, argValues, context);
             if (err.IsError)
             {
                 return (ValueNull.Instance, err);
             }
 
-            return (IndexOf(argValues[0]), NoError.Instance);
+            return (IndexOf(argValues[0]), ErrorNull.Instance);
         }
         return base.GetFunc(name, argValues);
     }
-    private Number IndexOf(Value value)
+    private VNumber IndexOf(Value value)
     {
         if (value == null) return new(-1);
 
         // Use pattern matching to simplify type checks and casts
-        var index = value switch
+        int index = value switch
         {
-            Number num => elements.FindIndex(v => v is Number n && n.value == num.value),
-            String str => elements.FindIndex(v => v is String s && s.value == str.value),
-            Bool b => elements.FindIndex(v => v is Bool boolVal && boolVal.value == b.value),
-            List list => elements.FindIndex(v => v is List l && l.elements.SequenceEqual(list.elements)),
+            VNumber num => elements.FindIndex(v => v is VNumber n && n.value == num.value),
+            VString str => elements.FindIndex(v => v is VString s && s.value == str.value),
+            VBool b => elements.FindIndex(v => v is VBool boolVal && boolVal.value == b.value),
+            VList list => elements.FindIndex(v => v is VList l && l.elements.SequenceEqual(list.elements)),
             _ => -1
         };
 
         return new(index);
     }
 
-    public override ValueError addedTo(Value other)
+    public override ValueError AddedTo(Value other)
     {
-        if (other is List _other)
+        if (other is VList _other)
         {
             elements.AddRange(_other.elements);
-            return (new List(elements), NoError.Instance);
+            return (new VList(elements), ErrorNull.Instance);
         }
         else
         {
             return (ValueNull.Instance, IlligalOperation(other));
         }
     }
-    public override ValueError muledTo(Value other)
+    public override ValueError MuledTo(Value other)
     {
-        if (other is Number _other)
+        if (other is VNumber _other)
         {
             List<Value> startElments = [.. elements];
             if (_other.value < 2)
             {
-                return (_other.value < 1 ? new List([]) : new List(elements), NoError.Instance); // * 1 = original * 0 or less = []
+                return (_other.value < 1 ? new VList([]) : new VList(elements), ErrorNull.Instance); // * 1 = original * 0 or less = []
             }
 
             for (int i = 2; i <= _other.value; i++)
@@ -123,7 +123,7 @@ public class List(List<Value> elements) : Value
                 elements.AddRange(startElments);
             }
 
-            return (new List(elements), NoError.Instance);
+            return (new VList(elements), ErrorNull.Instance);
         }
         else
         {
@@ -131,22 +131,22 @@ public class List(List<Value> elements) : Value
         }
     }
 
-    public override ValueError getComparisonEQ(Value other)
+    public override ValueError GetComparisonEQ(Value other)
     {
-        if (other is List _other)
+        if (other is VList _other)
         {
-            return (new Bool(elements == _other.elements), NoError.Instance);
+            return (new VBool(elements == _other.elements), ErrorNull.Instance);
         }
         else
         {
             return (ValueNull.Instance, IlligalOperation(other));
         }
     }
-    public override ValueError getComparisonNE(Value other)
+    public override ValueError GetComparisonNE(Value other)
     {
-        if (other is List _other)
+        if (other is VList _other)
         {
-            return (new Bool(elements != _other.elements), NoError.Instance);
+            return (new VBool(elements != _other.elements), ErrorNull.Instance);
         }
         else
         {
@@ -154,13 +154,13 @@ public class List(List<Value> elements) : Value
         }
     }
 
-    public override bool isTrue()
+    public override bool IsTrue()
     {
         return elements.Count > 0;
     }
-    public override Value copy()
+    public override Value Copy()
     {
-        List copy = new(elements);
+        VList copy = new(elements);
         copy.SetPos(startPos, endPos);
         copy.SetContext(context);
         return copy;
