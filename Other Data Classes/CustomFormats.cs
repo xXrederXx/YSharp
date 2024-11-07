1f03b035adceaea052ac3c1c9a90ca85e369f9e2
xXrederXx <150281252+xXrederXx@users.noreply.github.com>
2024-11-07 18:44:15 +0100

    Update CustomFormats.cs with records
    Change from read-only struct to record

--- Other Data Classes/CustomFormats.cs
+++ Other Data Classes/CustomFormats.cs
@@ -2,22 +2,15 @@
 
 namespace YSharp_2._0;
 
-public readonly struct ElseCaseData(INode? _node, bool? _bool){ // Node Bool Nullable
-    public static readonly ElseCaseData _null = new(null, null);
-    public readonly bool? Bool = _bool;
-    public readonly INode? Node = _node;
+public record ElseCaseData(INode? Node, bool? Bool){
+    public static ElseCaseData _null = new(null, null);
 }
+public record IfExpresionCases(INode condition, INode expression, bool returnNull){}
 
-public readonly struct IfExpresionCases(INode condition, INode expression, bool returnNull){
-    public readonly INode condition = condition;
-    public readonly INode expresion = expression;
-    public readonly bool returnNull = returnNull;
-}
-
-public readonly struct ValueError(Value _value, Error _error){
-    public readonly Value value = _value;
-    public readonly Error error= _error;
-    public readonly bool ValueIsNull => value is null or ValueNull;
+public record ValueError(Value _value, Error _error){
+    public Value value = _value;
+    public Error error= _error;
+    public bool ValueIsNull => value is null or ValueNull;
 
     public static implicit operator ValueError((Value, Error) other){
         return new ValueError(other.Item1, other.Item2);
