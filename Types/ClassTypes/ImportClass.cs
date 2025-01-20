d298c5579aa29731ac8b5eb99b8d9c0381038bfb
xXrederXx <amorgenthalerthierry@gmail.com>
2025-01-20 16:55:33 +0100

    GetFunc implementation

--- Types/ClassTypes/ImportClass.cs
+++ Types/ClassTypes/ImportClass.cs
@@ -16,67 +16,116 @@ public class ImportClass : Value
 
     public override ValueAndError GetFunc(string name, List<Value> argNodes)
     {
-        foreach(MethodInfo? method in data.methods){
+
+        ValueAndError returnVE = new(ValueNull.Instance, new FuncNotFoundError(
+            argNodes.Count >= 1 ? argNodes[0].startPos : Position.Null, 
+            $"No Function {name} found",
+            new Internal.Context()
+            )
+        );
+    
+        foreach (MethodInfo? method in data.methods)
+        {
             ParameterInfo[] paramsInfo = method.GetParameters();
 
-            if(method.Name != name && paramsInfo.Length != argNodes.Count)
+            if (method.Name != name && paramsInfo.Length != argNodes.Count)
+            {
+                continue;
+            }
+
+            if (!TypeCanBeUsed(method.ReturnType))
             {
                 continue;
             }
 
             bool canBeUsed = false;
-
+            List<object?> argList = new List<object?>();
 
             for (int i = 0; i < paramsInfo.Length; i++)
             {
-                ParameterInfo param = paramsInfo[i];
-                canBeUsed = TypeCanBeUsed(param.ParameterType);
-                if(!canBeUsed)
+                object? convParm = ConvertParam(paramsInfo[i].ParameterType, argNodes[i]);
+                canBeUsed = convParm is not null;
+                if (!canBeUsed)
                 {
                     break;
                 }
+                argList.Add(convParm);
             }
 
-            if (!canBeUsed){
-                continue;
-            }
-
-            if(!TypeCanBeUsed(method.ReturnType))
+            if (!canBeUsed)
             {
                 continue;
             }
 
-
+            object? ret = method.Invoke(data.instance, argList.ToArray());
+            returnVE = new(ConvertReturn(ret), ErrorNull.Instance);
         }
+        return returnVE;
     }
 
-    private static bool TypeCanBeUsed(Type type)
+    private static object? ConvertParam(Type typeReqiered, Value valToConvert)
     {
-        if (type == typeof(string))
+        if (!TypeCanBeUsed(typeReqiered) || valToConvert is not IDefaultConvertableValue)
         {
-            return true;
-        }
-        if (type == typeof(bool))
-        {
-            return true;
-
-        }
-        if (type == typeof(float))
-        {
-            return true;
-        }
-        if (type == typeof(int))
-        {
-            return true;
+            return null;
         }
 
+        if (
+            typeReqiered == typeof(string)
+            && valToConvert is IDefaultConvertableValue<string> convStr
+        )
+        {
+            return convStr.value;
+        }
+        if (typeReqiered == typeof(bool) && valToConvert is IDefaultConvertableValue<bool> convBl)
+        {
+            return convBl.value;
+        }
+        if (
+            typeReqiered == typeof(double)
+            && valToConvert is IDefaultConvertableValue<double> convDb
+        )
+        {
+            return convDb;
+        }
+        return null;
+    }
+
+    private static Value ConvertReturn(object? data)
+    {
+        if (data is string str)
+        {
+            return new VString(str);
+        }
+        if (data is bool bl)
+        {
+            return new VBool(bl);
+        }
+        if (data is double db)
+        {
+            return new VNumber(db);
+        }
+        return ValueNull.Instance;
+    }
+
+    private static bool TypeCanBeUsed(Type typeReqiered)
+    {
+        if (typeReqiered == typeof(string))
+        {
+            return true;
+        }
+        if (typeReqiered == typeof(bool))
+        {
+            return true;
+        }
+        if (typeReqiered == typeof(double))
+        {
+            return true;
+        }
 
         return false;
     }
-    private static bool TryGetValue<T>(Value val, out T value)
-    {
-        
-    }
+
     public override Value Copy()
     {
         return base.Copy();
