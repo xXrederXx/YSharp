152fc3d844b4086dd216dc92be1b9623b1f22cdd
xXrederXx <morgenthalerthierry@gmail.com>
2026-01-03 16:07:33 +0100

    feat(CliArgs): Extend class with iequatable and implement overrides

--- src/Common/CliArgs.cs
+++ src/Common/CliArgs.cs
@@ -2,7 +2,7 @@ using CommandLine;
 
 namespace YSharp.Common;
 
-public class CliArgs
+public class CliArgs : IEquatable<CliArgs>
 {
     [Option('o', "optimization", Required = false, Default = 0)]
     public int Optimization { get; set; }
@@ -23,4 +23,31 @@ public class CliArgs
         Default = null
     )]
     public string? ScriptPath { get; set; }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Optimization, RenderDot, ScriptPath);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not CliArgs cliArgs)
+            return false;
+        return Equals(cliArgs);
+    }
+
+    public bool Equals(CliArgs? other)
+    {
+        if (other is null)
+            return false;
+
+        return Optimization == other.Optimization
+            && RenderDot == other.RenderDot
+            && ScriptPath == other.ScriptPath;
+    }
+
+    public override string ToString()
+    {
+        return $"Optimize({Optimization}) RenderDot({RenderDot}) ScriptPath({ScriptPath})";
+    }
 }
