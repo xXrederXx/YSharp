9a02db331a13bed4b218d02c38713e2f59c3b0b5
xXrederXx <morgenthalerthierry@gmail.com>
2026-01-03 17:24:20 +0100

    fix(Benchmarks): update the lexer bench types to result

--- Benchmarks/LexerBench.cs
+++ Benchmarks/LexerBench.cs
@@ -4,40 +4,41 @@ using YSharp.Lexer;
 
 namespace YSharp.Benchmarks;
 
+using LexerResult = Result<List<BaseToken>, Error>;
 
 public class LexerBench
 {
-    public bool x;
+    public LexerResult x;
 
     [Benchmark]
     public void LexerBenchmarkL()
     {
         Lexer.Lexer lexer = new(BenchHelp.Text50000Char, "BENCHMARK");
-        (List<BaseToken>, Error) tokens = lexer.MakeTokens();
-        x = tokens.Item2.IsError;
+        LexerResult res = lexer.MakeTokens();
+        x = res;
     }
 
     [Benchmark]
     public void LexerBenchmarkM()
     {
         Lexer.Lexer lexer = new(BenchHelp.Text10000Char, "BENCHMARK");
-        (List<BaseToken>, Error) tokens = lexer.MakeTokens();
-        x = tokens.Item2.IsError;
+        LexerResult res = lexer.MakeTokens();
+        x = res;
     }
 
     [Benchmark]
     public void LexerBenchmarkS()
     {
         Lexer.Lexer lexer = new(BenchHelp.Text5000Char, "BENCHMARK");
-        (List<BaseToken>, Error) tokens = lexer.MakeTokens();
-        x = tokens.Item2.IsError;
+        LexerResult res = lexer.MakeTokens();
+        x = res;
     }
 
     [Benchmark]
     public void LexerBenchmarkXL()
     {
         Lexer.Lexer lexer = new(BenchHelp.Text100000Char, "BENCHMARK");
-        (List<BaseToken>, Error) tokens = lexer.MakeTokens();
-        x = tokens.Item2.IsError;
+        LexerResult res = lexer.MakeTokens();
+        x = res;
     }
 }
