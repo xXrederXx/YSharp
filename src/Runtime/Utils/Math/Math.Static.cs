4834d48afefd45ccace0ae4a5a51f30e339a6dc8
xXrederXx <morgenthalerthierry@gmail.com>
2026-04-05 11:34:28 +0200

    refactor(math): remove massive switch case

--- src/Runtime/Utils/Math/Math.Static.cs
+++ src/Runtime/Utils/Math/Math.Static.cs
@@ -11,27 +11,27 @@ public sealed partial class VMath : Value
     static VMath()
     {
         methodTable = new MethodTable<VMath>([
-            ("ABS", (self, args) => GetMathFunc(self, "ABS", args)),
-            ("CEIL", (self, args) => GetMathFunc(self, "CEIL", args)),
-            ("FLOOR", (self, args) => GetMathFunc(self, "FLOOR", args)),
-            ("ROUND", (self, args) => GetMathFunc(self, "ROUND", args)),
-            ("SQRT", (self, args) => GetMathFunc(self, "SQRT", args)),
-            ("CBRT", (self, args) => GetMathFunc(self, "CBRT", args)),
-            ("SIN", (self, args) => GetMathFunc(self, "SIN", args)),
-            ("COS", (self, args) => GetMathFunc(self, "COS", args)),
-            ("TAN", (self, args) => GetMathFunc(self, "TAN", args)),
-            ("SINH", (self, args) => GetMathFunc(self, "SINH", args)),
-            ("COSH", (self, args) => GetMathFunc(self, "COSH", args)),
-            ("TANH", (self, args) => GetMathFunc(self, "TANH", args)),
-            ("ASIN", (self, args) => GetMathFunc(self, "ASIN", args)),
-            ("ACOS", (self, args) => GetMathFunc(self, "ACOS", args)),
-            ("ATAN", (self, args) => GetMathFunc(self, "ATAN", args)),
-            ("ASINH", (self, args) => GetMathFunc(self, "ASINH", args)),
-            ("ACOSH", (self, args) => GetMathFunc(self, "ACOSH", args)),
-            ("ATANH", (self, args) => GetMathFunc(self, "ATANH", args)),
-            ("LOG", (self, args) => GetMathFunc(self, "LOG", args)),
-            ("LOG2", (self, args) => GetMathFunc(self, "LOG2", args)),
-            ("LOG10", (self, args) => GetMathFunc(self, "LOG10", args)),
+            ("ABS", (self, args) => GetMathFunc(self, System.Math.Abs, args)),
+            ("CEIL", (self, args) => GetMathFunc(self, System.Math.Ceiling, args)),
+            ("FLOOR", (self, args) => GetMathFunc(self, System.Math.Floor, args)),
+            ("ROUND", (self, args) => GetMathFunc(self, System.Math.Round, args)),
+            ("SQRT", (self, args) => GetMathFunc(self, System.Math.Sqrt, args)),
+            ("CBRT", (self, args) => GetMathFunc(self, System.Math.Cbrt, args)),
+            ("SIN", (self, args) => GetMathFunc(self, System.Math.Sin, args)),
+            ("COS", (self, args) => GetMathFunc(self, System.Math.Cos, args)),
+            ("TAN", (self, args) => GetMathFunc(self, System.Math.Tan, args)),
+            ("SINH", (self, args) => GetMathFunc(self, System.Math.Sinh, args)),
+            ("COSH", (self, args) => GetMathFunc(self, System.Math.Cosh, args)),
+            ("TANH", (self, args) => GetMathFunc(self, System.Math.Tanh, args)),
+            ("ASIN", (self, args) => GetMathFunc(self, System.Math.Asin, args)),
+            ("ACOS", (self, args) => GetMathFunc(self, System.Math.Acos, args)),
+            ("ATAN", (self, args) => GetMathFunc(self, System.Math.Atan, args)),
+            ("ASINH", (self, args) => GetMathFunc(self, System.Math.Asinh, args)),
+            ("ACOSH", (self, args) => GetMathFunc(self, System.Math.Acosh, args)),
+            ("ATANH", (self, args) => GetMathFunc(self, System.Math.Atanh, args)),
+            ("LOG", (self, args) => GetMathFunc(self, System.Math.Log, args)),
+            ("LOG2", (self, args) => GetMathFunc(self, System.Math.Log2, args)),
+            ("LOG10", (self, args) => GetMathFunc(self, System.Math.Log10, args)),
         ]);
         propertyTable = new PropertyTable<VMath>([
             ("PI", self => Result<Value, Error>.Success(new VNumber(System.Math.PI))),
@@ -40,7 +40,7 @@ public sealed partial class VMath : Value
         ]);
     }
 
-    private static Result<Value, Error> GetMathFunc(VMath self, string name, List<Value> argNodes)
+    private static Result<Value, Error> GetMathFunc(VMath self, Func<double, double> func, List<Value> argNodes)
     {
         Error err = ValueHelper.CheckArgs(
             argNodes,
@@ -51,53 +51,6 @@ public sealed partial class VMath : Value
         if (err.IsError)
             return Result<Value, Error>.Fail(err);
 
-        return name switch
-        {
-            "ABS" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Abs(((VNumber)argNodes[0]).value))),
-            "CEIL" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Ceiling(((VNumber)argNodes[0]).value))),
-            "FLOOR" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Floor(((VNumber)argNodes[0]).value))),
-            "ROUND" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Round(((VNumber)argNodes[0]).value))),
-            "SQRT" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Sqrt(((VNumber)argNodes[0]).value))),
-            "CBRT" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Cbrt(((VNumber)argNodes[0]).value))),
-            "SIN" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Sin(((VNumber)argNodes[0]).value))),
-            "COS" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Cos(((VNumber)argNodes[0]).value))),
-            "TAN" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Tan(((VNumber)argNodes[0]).value))),
-            "SINH" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Sinh(((VNumber)argNodes[0]).value))),
-            "COSH" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Cosh(((VNumber)argNodes[0]).value))),
-            "TANH" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Tanh(((VNumber)argNodes[0]).value))),
-            "ASIN" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Asin(((VNumber)argNodes[0]).value))),
-            "ACOS" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Acos(((VNumber)argNodes[0]).value))),
-            "ATAN" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Atan(((VNumber)argNodes[0]).value))),
-            "ASINH" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Asinh(((VNumber)argNodes[0]).value))),
-            "ACOSH" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Acosh(((VNumber)argNodes[0]).value))),
-            "ATANH" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Atanh(((VNumber)argNodes[0]).value))),
-            "LOG" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Log(((VNumber)argNodes[0]).value))),
-            "LOG2" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Log2(((VNumber)argNodes[0]).value))),
-            "LOG10" =>
-                Result<Value, Error>.Success(new VNumber(System.Math.Log10(((VNumber)argNodes[0]).value))),
-            _ => throw new NotImplementedException(
-                $"You called this with {name} but didnt implement it"
-            ),
-        };
+        return Result<Value, Error>.Success(new VNumber(func(((VNumber)argNodes[0]).value)));
     }
 }
