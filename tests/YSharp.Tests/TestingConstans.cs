4bf040f74840a4413689ee51102b553399ce52f9
xXrederXx <morgenthalerthierry@gmail.com>
2026-04-18 17:12:18 +0200

    test: require more precision

--- tests/YSharp.Tests/TestingConstans.cs
+++ tests/YSharp.Tests/TestingConstans.cs
@@ -2,6 +2,6 @@ namespace YSharp.Tests;
 
 public static class TestingConstans
 {
-    public const double DOUBLE_PRECISION = 1e-9;
-    public static readonly TimeSpan TIME_PRECISION = new TimeSpan(0, 0, 0, 0, 100);
+    public const double DOUBLE_PRECISION = 1e-12;
+    public static readonly TimeSpan TIME_PRECISION = new TimeSpan(0, 0, 0, 0, 50); // 50ms
 }
