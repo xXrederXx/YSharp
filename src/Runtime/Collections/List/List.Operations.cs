b87f20ba6d01cbbde38b2e5e91e0da284075330d
xXrederXx <morgenthalerthierry@gmail.com>
2026-04-23 18:56:04 +0200

    fix(VList): fix multo negative number now returns argoutofrange error

--- src/Runtime/Collections/List/List.Operations.cs
+++ src/Runtime/Collections/List/List.Operations.cs
@@ -4,7 +4,6 @@ using YSharp.Runtime.Primitives.Number;
 
 namespace YSharp.Runtime.Collections.List;
 
-
 public sealed partial class VList : Value
 {
     public override Result<Value, Error> AddedTo(Value other)
@@ -20,14 +19,16 @@ public sealed partial class VList : Value
 
     public override Result<Value, Error> GetComparisonEQ(Value other)
     {
-        if (other is VList _other) return Result<Value, Error>.Success(new VBool(value == _other.value));
+        if (other is VList _other)
+            return Result<Value, Error>.Success(new VBool(value == _other.value));
 
         return IllegalOperation(other);
     }
 
     public override Result<Value, Error> GetComparisonNE(Value other)
     {
-        if (other is VList _other) return Result<Value, Error>.Success(new VBool(value != _other.value));
+        if (other is VList _other)
+            return Result<Value, Error>.Success(new VBool(value != _other.value));
 
         return IllegalOperation(other);
     }
@@ -37,10 +38,26 @@ public sealed partial class VList : Value
         if (other is VNumber _other)
         {
             List<Value> startElments = [.. value];
-            if (_other.value < 2)
-                return Result<Value, Error>.Success(_other.value < 1 ? new VList([]) : new VList(value)); // * 1 = original * 0 or less = []
-
-            for (int i = 2; i <= _other.value; i++) value.AddRange(startElments);
+            if (_other.value == 1)
+            {
+                return Result<Value, Error>.Success(new VList(value));
+            }
+            if (_other.value == 0)
+            {
+                return Result<Value, Error>.Success(new VList([]));
+            }
+            if (_other.value < 0)
+            {
+                return Result<Value, Error>.Fail(
+                    new ArgOutOfRangeError(
+                        _other.StartPos,
+                        "The multiplier of the list must be greater or equal to 0",
+                        new Context()
+                    )
+                );
+            }
+            for (int i = 2; i <= _other.value; i++)
+                value.AddRange(startElments);
 
             return Result<Value, Error>.Success(new VList(value));
         }
